0f27d972fc9e06aae095c82e14bbf0da6cb43027
vivektarun <vivektarun1234@gmail.com>
2024-05-30 21:11:28 +0530

    feat: status code added

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -1,35 +1,36 @@
 const { TodosRepository } = require("../repositories");
 const { TodosService } = require("../services");
+const { StatusCodes } = require('http-status-codes');
 
 const todosService = new TodosService(new TodosRepository());
 
 async function pingtodosController(req, res) {
-  return res.json({ message: "pong" });
+  return res.status(StatusCodes.OK).json({ message: "pong" });
 }
 
 async function getTodos(req, res) {
   const todo = await todosService.getTodos(req.params.id);
-  return res.json(todo);
+  return res.status(StatusCodes.OK).json(todo);
 }
 
 async function getAllTodos(req, res) {
   const allTodos = await todosService.getAllTodos();
-  return res.json(allTodos);
+  return res.status(StatusCodes.OK).json(allTodos);
 }
 
 async function addTodos(req, res) {
   const newTodo = await todosService.addTodos(req.body.text);
-  return res.json(newTodo);
+  return res.status(StatusCodes.CREATED).json(newTodo);
 }
 
 async function deleteTodos(req, res) {
   const remainingTodos = await todosService.deleteTodos();
-  return res.json(remainingTodos);
+  return res.status(StatusCodes.OK).json(remainingTodos);
 }
 
 async function deleteByIdTodos(req, res) {
   const deletedTodo = await todosService.deleteByIdTodos(req.params.id);
-  return res.json(deletedTodo);
+  return res.status(StatusCodes.OK).json(deletedTodo);
 }
 
 module.exports = {
